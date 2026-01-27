61d9fc4b033e0bad34f7b8d07e03eb86b9bc5cfe
Ymesfun <ymesf295@mtroyal.ca>
2026-01-27 11:41:18 -0700

    Adds the intermediatecode class to the intermediate.py file, adds a basic operatorprinter method
    TODO: Test cases, potential hidden bugfixes (code rn is very small and compact so everything should be easy and simple.)

--- Project 1/intermediate.py
+++ Project 1/intermediate.py
@@ -35,6 +35,33 @@ class Operation:
             return f"{self.destination} = -{self.operand1}"
         return f"{self.destination} = {self.operand1} {self.operator} {self.operand2}"
 
+#Purpose: This takes a line and converts it into tokens, which are returned in an operator 
+#         class. Supports assignment inputs (e.g. x = a). 
+# 
+#Assumptions:
+#   Assumes tokens are stored in infix notation e.g. x x + 1
+#   Assumes that quick operators are decomposed and broken into their subparts
+#   (e.g. x += 1 is changed to x = x + 1)
+#   Assumes that tokens have superfluous operators removed, e.g. no equality if not an assignment operator
+#
+#Input: A tokenized string, given as a list. List expected to have the format below:
+#       [destination, operand1, operator, optional:operand2]
+#
+#Output: A member of the Operation class, with a destination, operation, and operator(s)
+def TokenOperizer(tokens: List[str])->Operation:
+    op = Operation(tokens[0], tokens[2], tokens[1])
+    if(len(tokens)>3):
+        op.operand2 = tokens[3]
+    return op
+
+#Purpose: This Simply takes a list of all the operators and operations and prints them out.
+def OperatorPrinter(operators: List[str]) -> None:
+    for operation in operators:
+        if(operation.operator == "="):
+            print(f"{operation.destination} = {operation.operand1}") #if(operation.operand2 != None):
+        else:
+            print(f"{operation.destination} = {operation.operand1} {operation.operator} {operation.operand2}")
+    
 
 class IntermediateCode: # TO DO
     """
@@ -47,3 +74,11 @@ class IntermediateCode: # TO DO
     b  = t2 - a     ← one operation
     live: b
     """
+    def __init__(self, op:Operation = None, oplist: List[Operation] = []):
+        if op is not None:
+            self.oplist.append(op)
+    #This method is just a lazy and simple abstraction to insert an operation.
+    def insert(self, op:Operation):
+        self.oplist.append(op)
+
+        
