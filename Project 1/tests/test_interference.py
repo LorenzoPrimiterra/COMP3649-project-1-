d60cd72890a702968cf02c1048c0755f546b7237
Ymesfun <ymesf295@mtroyal.ca>
2026-04-12 22:41:12 -0600

    Adds extra test cases

--- Project 1/tests/test_interference.py
+++ Project 1/tests/test_interference.py
@@ -1,6 +1,6 @@
 import io
 from parser import readIntermediateCode
-from interference import build_interference_graph
+from interference import build_interference_graph, allocate_registers
 
 
 def test_no_interference():
@@ -31,3 +31,34 @@ live: a, b
     # a interferes with t1
     assert "t1" in graph.get_neighbors("a")
     assert "a" in graph.get_neighbors("t1")
+
+def test_colouring_success():
+    program = """a = 1
+b = 2
+c = a + b
+live: c
+"""
+    code = readIntermediateCode(io.StringIO(program))
+    code.compute_liveness_info()
+
+    graph = build_interference_graph(code)
+
+    success = allocate_registers(graph, 2)
+
+    assert success is True
+    assert len(graph.assignments) == len(graph.nodes)
+
+def test_colouring_fail():
+    program = """a = 1
+b = 2
+c = a + b
+live: c
+"""
+    code = readIntermediateCode(io.StringIO(program))
+    code.compute_liveness_info()
+
+    graph = build_interference_graph(code)
+
+    success = allocate_registers(graph, 1)
+
+    assert success is False
